ec8d7b130b5765a928eec307e91b0e074a9ff838
Matt Gibbs <mgibbs@slac.stanford.edu>
2017-02-28 15:46:44 -0800

    Increment version number to 0.2.1.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='meme',
-    version='0.2.0',
+    version='0.2.1',
     description='A wrapper for the MEME services at SLAC.',
     long_description=long_description,
 
