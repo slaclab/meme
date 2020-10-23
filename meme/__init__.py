1b8afa3db5cdcdf4565d9b9b6dffc9f301503afe
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-23 14:18:45 -0700

    FIX: Patch p4p close to avoid issues with destructor.

--- meme/__init__.py
+++ meme/__init__.py
@@ -1,2 +1,16 @@
+# Patch p4p close with Python 2.7
+# For more details see: https://github.com/mdavidsaver/p4p/issues/55
+from p4p.client import thread
+
+_p4p_thread_context_close = thread.Context.close
+
+def close(*args, **kwargs):
+    try:
+        _p4p_thread_context_close(*args, **kwargs)
+    except TypeError:
+        pass
+
+thread.Context.close = close
+
 from . import names
 from . import archive
